861cbb327eab280fffc7260a75731c36e0466213
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-10-19 22:30:00 -0700

    WIP

--- sanity/schemas/objects/linkCard.js
+++ sanity/schemas/objects/linkCard.js
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from 'react'
+
+const Preview = ({ value }) => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const response = async () => {
+      let url = 'https://api.github.com/repos/rosnovsky/rosnovskyus/commits'
+      let response = await fetch(url)
+
+      let commits = await response.json() // read response body and parse as JSON
+
+      setData(commits[0].commit.author.name)
+      console.warn(commits[0].commit.author.name)
+      return commits[0].commit.author.name
+    }
+    response()
+  }, [])
+  return <p>{data}</p>
+}
+
 export default {
   title: 'Link Card',
   name: 'linkCard',
   type: 'object',
+  preview: {
+    select: {
+      url: 'href'
+    },
+    component: Preview
+  },
   fields: [
     {
       title: 'URL',
@@ -11,8 +38,48 @@ export default {
     },
     {
       title: 'Title',
-      name: 'blank',
-      type: 'string'
+      name: 'title',
+      type: 'string',
+      readOnly: true
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'string',
+      readOnly: true
+    },
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'string',
+      readOnly: true
+    },
+    {
+      title: 'Publisher',
+      name: 'publisher',
+      type: 'string',
+      readOnly: true
+    },
+    {
+      title: 'Logo URL',
+      name: 'logo',
+      type: 'url',
+      readOnly: true
+    },
+    {
+      title: 'Image URL',
+      name: 'imageUrl',
+      type: 'url',
+      readOnly: true
+    },
+    {
+      title: 'Publication Date',
+      name: 'publicationDate',
+      type: 'datetime',
+      options: {
+        dateFormat: 'MMM Do, YYYY'
+      },
+      readOnly: true
     }
   ]
 }
