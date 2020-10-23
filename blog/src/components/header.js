21dfb4ef274cb04791ff355a1732042e69d350d0
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-10-22 20:33:37 -0700

    wip

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -18,7 +18,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
           </a>
         </div>
         <div className="-mr-2 -my-2 md:hidden">
-          <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+          <button onClick={(e => isOn ? setIsOn(false) : setIsOn(true))} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
 
             <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -48,7 +48,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
         </div>
       </div>
     </div>
-    <div onClick={(e => console.log(e))} className={`absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden`}>
+    <div className={`${isOn ? "visible" : "hidden"} absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden`}>
       <div className="rounded-lg shadow-lg">
         <div className="rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50">
           <div className="pt-5 pb-6 px-5 space-y-6">
@@ -57,7 +57,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
                 <img className="h-8 w-auto" src="https://rosnovsky.us/favicon.png" alt="Workflow" />
               </div>
               <div className="-mr-2">
-                <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button onClick={(e => isOn ? setIsOn(false) : setIsOn(true))} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
 
                   <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -73,7 +73,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                   </svg>
                   <div className="text-base leading-6 font-medium text-gray-900">
-                    Analytics
+                    Archive
                   </div>
                 </a>
                 <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
@@ -82,7 +82,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
                   </svg>
                   <div className="text-base leading-6 font-medium text-gray-900">
-                    Engagement
+                    About Me
                   </div>
                 </a>
                 <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
@@ -91,25 +91,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                   </svg>
                   <div className="text-base leading-6 font-medium text-gray-900">
-                    Security
-                  </div>
-                </a>
-                <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-
-                  <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                  </svg>
-                  <div className="text-base leading-6 font-medium text-gray-900">
-                    Integrations
-                  </div>
-                </a>
-                <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-
-                  <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                  </svg>
-                  <div className="text-base leading-6 font-medium text-gray-900">
-                    Automations
+                    Projects
                   </div>
                 </a>
               </nav>
@@ -118,13 +100,13 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
           <div className="py-6 px-5 space-y-6">
             <div className="space-y-6">
               <span className="w-full flex rounded-md shadow-sm">
-                <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-orange-600 hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700 transition ease-in-out duration-150">
                   Sign up
                 </a>
               </span>
               <p className="text-center text-base leading-6 font-medium text-gray-500">
-                Already a member?
-                <a href="#" className="text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150">
+                Already a member? <br />
+                <a href="#" className="text-orange-600 hover:text-orange-500 transition ease-in-out duration-150">
                   Sign in
                 </a>
               </p>
@@ -135,7 +117,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
     </div>
   </div>
 
-  <main className="lg:relative">
+  {/* <main className="lg:relative">
     <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
       <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
         <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
@@ -163,7 +145,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => {
     <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
       <img className="absolute inset-0 w-full h-full object-cover" src="https://s3.us-west-1.wasabisys.com/rosnovsky-media/IMG_0700.jpeg" alt="Woman on her phone" />
     </div>
-  </main>
+  </main> */}
 </div>
 }
 
