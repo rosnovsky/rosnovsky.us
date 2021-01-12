0b218324d5eeec58f9d8850c2b1ea622c5d75e5e
Art Rosnovsky <art.rosnovsky@auth0.com>
2021-01-11 18:48:14 -0800

    Updated Login button as part of #21

--- blog/components/Comments/CommentLogin.tsx
+++ blog/components/Comments/CommentLogin.tsx
@@ -6,7 +6,7 @@ const CommentLogin = () => {
           type="submit"
           className="flex-inline py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 self-end"
         >
-          Submit
+          Login to Comment
         </button>
       </div>
     </div>
