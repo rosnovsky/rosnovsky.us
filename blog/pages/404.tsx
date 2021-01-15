980016e3db81739c0db5d9f400574c7d6807d20b
Art Rosnovsky <art.rosnovsky@auth0.com>
2021-01-14 16:12:30 -0800

    oops

--- blog/pages/404.tsx
+++ blog/pages/404.tsx
@@ -37,26 +37,27 @@ const NotFound = ({ menuItems }: any) => {
         canonicalUrl="https://rosnovsky.us/"
         coverAlt="Rosnovsky Park"
       />
-      <Layout menuItems={menuItems}></Layout>
-      <div className="">
-        <Covid />
-        <div className="bg-white h-auto ">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-            <div className="text-center">
-              <h2 className="text-base font-semibold text-orange font-mono tracking-wide uppercase mb-5">
-                Error 404
-              </h2>
-              <p className="mt-1 text-4xl font-black font-mono text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl pb-5">
-                Page Not Found
-              </p>
-              <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-                Let's not point fingers, ok? Ok.
-              </p>
+      <Layout menuItems={menuItems}>
+        <div className="">
+          <Covid />
+          <div className="bg-white h-auto ">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+              <div className="text-center">
+                <h2 className="text-base font-semibold text-orange font-mono tracking-wide uppercase mb-5">
+                  Error 404
+                </h2>
+                <p className="mt-1 text-4xl font-black font-mono text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl pb-5">
+                  Page Not Found
+                </p>
+                <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                  Let's not point fingers, ok? Ok.
+                </p>
+              </div>
             </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Layout>
     </>
   )
 }
