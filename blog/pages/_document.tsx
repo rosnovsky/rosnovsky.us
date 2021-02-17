7ef86c0b0e6edfc9f84e2585ff30c33de85c97a1
Art Rosnovsky <art.rosnovsky@auth0.com>
2021-02-17 10:19:11 -0800

    more font optimizations

--- blog/pages/_document.tsx
+++ blog/pages/_document.tsx
@@ -4,7 +4,32 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="preconnect"
+            href="https://api.covidtracking.com/v1/us/current.json"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preconnect"
+            href="https://vitals.vercel-insights.com"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preconnect"
+            href="https://cdn.usefathom.com"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preconnect"
+            href="https://img3.usefathom.com"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
