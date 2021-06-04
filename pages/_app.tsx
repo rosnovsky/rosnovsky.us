990ef236aad45cb98d60afe63374f4d8e00cac31
Art Rosnovsky <art.rosnovsky@auth0.com>
2021-06-03 17:59:31 -0700

    login fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
   useAnalytics();
 
   return (
-    <UserProvider profileUrl={""}>
+    <UserProvider>
       <ThemeProvider attribute="class">
         <MDXProvider components={MDXComponents}>
           <Component {...pageProps} />
