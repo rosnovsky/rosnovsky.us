3dcbc6ab5c22b80dbd73573dde050a0ceb3279b6
Art Rosnovsky <art.rosnovsky@auth0.com>
2021-07-15 15:18:52 -0700

    comments and profile updates

--- pages/api/comments/postComment.ts
+++ pages/api/comments/postComment.ts
@@ -2,9 +2,12 @@ import { supabase } from '../../../lib/supabase';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 import md5 from 'md5'
+import { userProfile } from './userProfile'
 
-const isCommentUnique = async (postId, content, userId) => {
+const isCommentUnique = async (postId, content, user) => {
   const commentHash = md5(content);
+  const userId = user.sub
+  console.log("🚀 ~ file: postComment.ts ~ line 10 ~ isCommentUnique ~ userId", userId)
   const { data, error } = await supabase
   .from('comments')
   .select('*')
@@ -12,26 +15,34 @@ const isCommentUnique = async (postId, content, userId) => {
 
   // check if the comment is unique (matching comment content hash, userId and postId)
   const commentsByUserId = data.filter(comment => comment.user_id === userId);
+
+  console.log("🚀 ~ file: postComment.ts ~ line 17 ~ isCommentUnique ~ commentsByUserId", commentsByUserId)
+  
   const isUnique = commentsByUserId.every(comment => comment.hash !== commentHash);
+
+  console.log("🚀 ~ file: postComment.ts ~ line 21 ~ isCommentUnique ~ isUnique", isUnique)
+  
   return isUnique
 }
 
 
-const postComment = async (postId, content, userId) => {
+const postComment = async (postId, content, user) => {
+  const userId = user.sub
+  await userProfile(user)
   const { data, error } = await supabase
   .from('comments')
-  .insert(
-    { user_id: userId, post_id: postId, comment: content, hash: md5(content)}, 
+  .upsert(
+    { user_id: userId, post_id: postId, comment: content, hash: md5(content)}, { onConflict: 'user_id', ignoreDuplicates: true } 
   )
+  console.log("🚀 ~ file: postComment.ts ~ line 34 ~ postComment ~ data", data)
   return error ? error : data;
 }
 
 export default withApiAuthRequired(async function (req: NextApiRequest, res: NextApiResponse) {
   // TODO: Handle errors, validation, etc.
   const session = getSession(req, res);
-  console.log(session)
 
-  return isCommentUnique(req.query.postId, req.body.content, session.user.sub) ? res.status(200).send(await postComment(req.query.postId, req.query.content, session.user.sub)) : res.status(400).send('Comment already exists');
+  return await isCommentUnique(req.query.postId, req.query.content, session.user) ? res.status(200).send(await postComment(req.query.postId, req.query.content, session.user)) : res.status(400).send('Comment already exists');
 
 })
 
--- pages/api/comments/userProfile.ts
+++ pages/api/comments/userProfile.ts
@@ -1,9 +1,9 @@
 import { supabase } from '../../../lib/supabase';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { UserProfile } from '@auth0/nextjs-auth0';
 
-const userProfile = async (user) => {
+export const userProfile = async (user: UserProfile) => {
   const {given_name, nickname, sub, family_name, name, picture, email, email_verified} = user
+  
   const { data, error } = await supabase
   .from('users')
   .upsert(
@@ -13,16 +13,9 @@ const userProfile = async (user) => {
     name,
     picture,
     email,
-    email_verified}, {onConflict: 'user_id'}
+    email_verified}, {onConflict: 'user_id', ignoreDuplicates: true}
   )
+  
   return error ? error : data;
 }
-
-export default withApiAuthRequired(async function (req: NextApiRequest, res: NextApiResponse) {
-  // TODO: Handle errors, validation, etc.
-  const session = getSession(req, res);
-  console.log(session)
-  
-  return res.status(200).send(await userProfile(session.user));
-})
 
