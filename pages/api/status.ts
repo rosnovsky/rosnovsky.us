4497824f53e8b8c8f52dd8c1e058b65265a5928a
Art Rosnovsky <artem@rosnovsky.us>
2021-08-14 22:53:07 -0700

    status init

---
+++ pages/api/status.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const getStatus = async () => {
+  return fetch('https://betteruptime.com/api/v2/monitors/395442', {
+    headers: {'Authorization': `Bearer ${process.env.STATUS}`}
+  })
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const result = await getStatus();
+  return res.status(200).send({result})
+}
+
