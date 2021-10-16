0cdfdfcfab7c125903504cbfaa16052992f4fb39
Art Rosnovsky <artem@rosnovsky.us>
2021-10-16 14:24:42 -0700

    Update About page (#184)

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-  import { UserProfile } from "@auth0/nextjs-auth0"
-  
-  export type PictureDescription = {
+import { UserProfile } from "@auth0/nextjs-auth0"
+
+export type PictureDescription = {
   description: {
     tags: string[]
     captions:
-      {
-        text: string
-        confidence: number
-      }[]
+    {
+      text: string
+      confidence: number
+    }[]
   },
   requestId: string
   metadata: {
@@ -49,5 +49,47 @@ type PostComment = {
   hash: string
 }
 
-type Status = {
-  data: {attributes: {status: 'string' }}}
+interface Status {
+  data: {
+    id: string
+    type: string
+    attributes: {
+      url: string
+      pronounceable_name: string
+      monitor_type: string
+      monitor_group_id?: string,
+      last_checked_at: string
+      status: "up" | "maintenance" | "validating" | "down" | "paused" | "pending"
+      policy_id?: string
+      required_keyword?: string
+      verify_ssl: boolean
+      check_frequency: number
+      call: boolean
+      sms: boolean
+      email: boolean
+      push: boolean
+      team_wait: boolean
+      http_method: "get" | "post"
+      request_timeout: number
+      recovery_period: number
+      request_headers: string[]
+      request_body: string
+      follow_redirects: boolean
+      remember_cookies: boolean
+      paused_at?: string
+      created_at: string
+      updated_at: string
+      ssl_expiration: string
+      domain_expiration: string
+      regions: string[]
+      port: number
+      confirmation_period: number
+    },
+    relationships: {
+      policy: {
+        data: string
+      }
+    }
+  }
+}
+
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,20 @@
+import Quote from '@components/Utils/Quote';
 import SubscribeCard from '../components/Cards/SubscribeCard';
 
 import Container from '../components/Container';
 
-const Talk = ({ title, link, children }) => (
+const ExternalLink = ({ href, children }) => (
+  <a
+    className="text-gray-900 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-500 transition"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}
+  >
+    {children}
+  </a>
+);
+
+const Podcast = ({ title, link, children }) => (
   <>
     <h3 className="font-medium mb-2 text-lg">
       <a
@@ -41,32 +53,45 @@ export default function About() {
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
         </h1>
-        <div className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
-        </div>
-        <h2 className="font-bold text-3xl tracking-tight mb-4 text-black dark:text-white">
+
+        <p className="text-gray-600 dark:text-gray-400 mt-5 mb-8">Hey, my name is Art.</p>
+
+        <p className="text-gray-600 dark:text-gray-400 mb-4">I'm a web developer based off Western Washington. I live about 50 miles north of Seattle in a small town called Arlington.</p>
+
+        <Quote
+          quote={"For a while, I lived in London (not that one, but the one in Ontario, Canada) before moving to Vancouver (not that one, but the one in Washington; not that Washington, but Washington state). Now I live in Arlington (and not the one in Virgina, mind you!)"}
+        />
+
+        <p className="text-gray-600 dark:text-gray-400 mb-4">After short gigs at Intel, AT&T, and Microsoft, I ended up at <ExternalLink href="https://auth0.com">Auth0</ExternalLink> (aquired by <ExternalLink href="https://okta.com">Okta</ExternalLink> in May 2021). Here I work as a Customer Onboarding Engineer, building out Auth0's enterprise customer onboarding program and tooling.</p>
+
+        <p className="text-gray-600 dark:text-gray-400 mb-4">Feel free to connect with me on <ExternalLink href="https://www.linkedin.com/in/rosnovsky/">LinkedIn</ExternalLink> or follow me on <ExternalLink href="https://www.twitter.com/rosnovsky/">Twitter</ExternalLink>.</p>
+
+        <Quote quote="For the longest time, I'd been a podcaster. In my previous life, I even was a minor radio celebrity, worked as a television producer and correspondent, and have done a bunch of front-line reporting from terrorist attacks, riots, and hostage situations. Yet here we are." />
+
+        <h2 className="font-bold text-3xl tracking-tight my-4 text-black dark:text-white">
           Podcasts in Russian // Подкасты на русском
         </h2>
-        <Talk
+        <Podcast
           title="Rosnovsky Park™ Weekly"
           link="https://gumroad.com/rosnovsky#gdTD"
         >
           Все доступные эпизоды подкаста Rosnovsky Park™ Weekly, одного из старейших подкастов на русском языке.
-        </Talk>
-        <Talk
+        </Podcast>
+        <Podcast
           title="Rosnovsky In Canada"
           link="https://gumroad.com/rosnovsky#iYQx"
         >
           Все 39 эпизодов подкаста Rosnovsky in Canada. Как получить визу в Канаду, как приехать учиться и остаться, на что похожа учеба и жизнь семьи в Канаде, где и как может учиться ребенок, на что посмотреть, куда съездить и так далее.
-        </Talk>
-        <Talk
+        </Podcast>
+        <Podcast
           title="Evergreen Podcast"
           link="https://gumroad.com/rosnovsky#qaxYX"
         >
           <p>Самый аутентичный подкаст на русском языке!</p>
 
           <p>Легендарный ведущий старейших подкастов на русском языке представляет третью серию подкастов — Вечнозелёный подкаст из вечнозеленого штата Вашингтон, что на Тихоокеанском Северо-Западе США 🇺🇸. Путешествия, походы, природа, работа, технологии, семья, дети, деньги — всё, что волнует интересует!</p>
-        </Talk>
-      <SubscribeCard />
+        </Podcast>
+        <SubscribeCard />
       </div>
     </Container>
   );
