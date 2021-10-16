bbc91e80c07dfc2507562fbec26a8894e6932673
Art Rosnovsky <artem@rosnovsky.us>
2021-10-16 14:40:41 -0700

    Comments styling (#185)
    * Update About page
    
    * fixed comments in dark mode

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -30,15 +30,15 @@ export default function Blog({ mdxSource, tweets, frontMatter, comments }: { mdx
       />
       {user ? <span id="comments" className="font-bold">
         <div className="flex mx-auto items-center justify-center shadow-lg mb-4 w-full">
-          <form className="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+          <form className="w-full max-w-xl bg-white dark:bg-gray-900 rounded-lg px-4 pt-2">
             <div className="flex flex-wrap -mx-3 mb-6">
-              <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
+              <h2 className="px-4 pt-1 pb-2 text-gray-800 dark:text-gray-200 text-lg">Add a new comment</h2>
               <div className="w-full md:w-full px-3 mb-2 mt-2">
-                <textarea className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Type Your Comment' required></textarea>
+                <textarea className="bg-gray-100 dark:bg-gray-700 rounded border dark:border-gray-700 border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 dark:placeholder-gray-200 focus:outline-none focus:bg-white dark:focus:bg-gray-900" name="body" placeholder='Type Your Comment' required></textarea>
               </div>
               <div className="w-full flex px-3">
                 <div className="w-full mx-auto">
-                  <input type='submit' disabled className="bg-white text-gray-700 font-medium py-1 px-4 mx-auto border border-gray-400 rounded-lg tracking-wide hover:bg-gray-100" value='Coming Soon!' />
+                  <input type='submit' disabled className="bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 font-medium py-1 px-4 mx-auto border dark:border-gray-600 border-gray-400 rounded-lg tracking-wide dark:hover:bg-gray-900 hover:bg-gray-100" value='Coming Soon!' />
                 </div>
               </div>
             </div>
