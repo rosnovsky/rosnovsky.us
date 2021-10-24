df4dc9951c0ae1e5b101b2cbd17f8e251f9385bf
Art Rosnovsky <artem@rosnovsky.us>
2021-10-24 11:18:33 -0700

    Fathom goal removed

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -4,7 +4,6 @@ import { trackGoal } from 'fathom-client'
 export default handleAuth({
   async login(req, res) {
     try {
-        trackGoal('UIIDM49K', 0)
         await handleLogin(req, res, { returnTo: `${req.headers.referer}` });
     } catch (error) {
       res.status(error.status || 500).end(error.message);
