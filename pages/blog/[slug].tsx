461a6c272b0d8e1b4925c3402bbdf76bdb4bf336
Art Rosnovsky <artem@rosnovsky.us>
2021-11-05 22:30:00 -0700

    `Cascadia 2021` post

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -115,16 +115,16 @@ export async function getStaticProps({ params }) {
     cover: post.frontMatter.cover ? post.frontMatter.cover : "https://rosnovsky.us/static/favicons/favicon.ico"
   }
 
-  // await fetch("https://rosnovsky.us/api/algoliasearch", {
-  //   method: 'POST',
-  //   body: JSON.stringify(index),
-  // })
+  await fetch("https://rosnovsky.us/api/algoliasearch", {
+    method: 'POST',
+    body: JSON.stringify(index),
+  })
 
 
-  const comments: PostComment[] = await fetch(process.env.NODE_ENV !== "production" ? `http://localhost:3000/api/comments/getComments?id=${params.slug}` : `https://rosnovsky.us/api/comments/getComments?id=${params.slug}`, {
-    method: 'GET',
-  }).then(res => res.json())
+  // const comments: PostComment[] = await fetch(process.env.NODE_ENV !== "production" ? `http://localhost:3000/api/comments/getComments?id=${params.slug}` : `https://rosnovsky.us/api/comments/getComments?id=${params.slug}`, {
+  //   method: 'GET',
+  // }).then(res => res.json())
 
 
-  return { props: { ...post, comments }, revalidate: 1 };
+  return { props: { ...post, comments: [] }, revalidate: 1 };
 }
