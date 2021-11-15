ca18be45ecd898bf91a770a3aacc9c72cda5f0fc
Art Rosnovsky <artem@rosnovsky.us>
2021-11-14 21:52:49 -0800

    excluded cypress from jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/tests/e2e/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/tests/cypress/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
       '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
