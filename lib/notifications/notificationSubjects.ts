3540dcf1139b3868fe19f8b6b641b0f0d7eb9331
Art Rosnovsky <artem@rosnovsky.us>
2021-12-28 13:21:13 -0800

    `2021: The Year in Review` post

--- lib/notifications/notificationSubjects.ts
+++ lib/notifications/notificationSubjects.ts
@@ -1,10 +1,11 @@
 import { NotifyOptions } from '.';
 
 export const selectSubject = (type: NotifyOptions['type']) => {
-
   switch (type) {
-    case 'new-comment-notification' || 'notify_subscriber':
-      return "New comment on "
+    case 'new-comment-notification':
+      return 'New comment on ';
+    case 'notify_subscriber':
+      return 'New comment on ';
     case 'flagged_comment':
       return `You flagged a comment on `;
     case 'deleted_comment':
@@ -12,4 +13,4 @@ export const selectSubject = (type: NotifyOptions['type']) => {
     default:
       return `Rosnovsky Park™ notification`;
   }
-}
+};
