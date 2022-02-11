d061a6e8e51c59c69fa010f2d3ca10279a2d7ef7
Art Rosnovsky <artem@rosnovsky.us>
2022-02-10 21:59:16 -0800

    Sanity init

---
+++ sanity/schemas/page.ts
@@ -0,0 +1,58 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+          options: { isHighlighted: true },
+        },
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          options: { isHighlighted: true },
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'coverImage',
+    },
+  },
+};
---
+++ sanity/schemas/post.ts
@@ -0,0 +1,95 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover image',
+      type: 'image',
+      fields: [
+        {
+          title: 'Caption',
+          name: 'caption',
+          type: 'string',
+          options: {
+            isHighlighted: true,
+          },
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: 'Alternative text',
+          name: 'alt',
+          type: 'string',
+          options: { isHighlighted: true },
+          validation: (Rule) => Rule.required(),
+        },
+      ],
+      options: {
+        hotspot: true,
+        metadata: ['exif', 'location', 'lqip', 'blurhash', 'palette'],
+      },
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'summary',
+      title: 'Summary',
+      type: 'blockContent',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'references',
+      title: 'Refernce Posts',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'post' } }],
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      publishedAt: 'publishedAt',
+      media: 'coverImage',
+    },
+    prepare(selection) {
+      const { publishedAt } = selection;
+      return Object.assign({}, selection, {
+        subtitle: new Date(publishedAt).toLocaleDateString(),
+      });
+    },
+  },
+};
