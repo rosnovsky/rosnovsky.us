d91d3b7203da631257ecb0e10f480052634262f0
Art Rosnovsky <art.rosnovsky@auth0.com>
2022-05-02 10:22:02 -0700

    cleanup

--- web/lib/helpers.tsx
+++ web/lib/helpers.tsx
@@ -1,9 +1,14 @@
-import { getImageDimensions } from '@sanity/asset-utils';
+import {
+  getImage,
+  getImageAsset,
+  getImageDimensions,
+} from '@sanity/asset-utils';
 import imageUrlBuilder from '@sanity/image-url';
 import sanityClient from './sanityClient';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { SanityImageObject } from '@sanity/image-url/lib/types/types';
-import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
+const SyntaxHighlighter = dynamic(() => import('react-syntax-highlighter'));
 import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import getYouTubeId from 'get-youtube-id';
 
@@ -23,20 +28,25 @@ export const PortableTextComponents = {
   types: {
     image: ({ value }: { value }) => {
       if (!value?.asset) return null;
+      console.log(getImage(value, { projectId: 'n3o7a5dl', dataset: 'prod' }));
       return (
-        <Image
-          src={urlFor(value).fit('crop').auto('format').url()}
-          width={getImageDimensions(value).width}
-          height={getImageDimensions(value).height}
-          loading="lazy"
-          placeholder="blur"
-          blurDataURL={urlFor(value)
-            .fit('max')
-            .auto('format')
-            .quality(10)
-            .blur(10)
-            .url()}
-        />
+        <div className="h-full w-full">
+          <Image
+            src={urlFor(value).fit('crop').auto('format').url()}
+            width={getImageDimensions(value).width}
+            height={getImageDimensions(value).height}
+            loading="lazy"
+            layout="responsive"
+            placeholder="blur"
+            blurDataURL={urlFor(value)
+              .fit('max')
+              .auto('format')
+              .quality(10)
+              .blur(10)
+              .url()}
+          />
+          <div className="mb-5">{value.alt}</div>
+        </div>
       );
     },
     code: ({ value }) => {
