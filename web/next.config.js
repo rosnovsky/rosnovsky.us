5bc34711aaa79363f45c672fcba86bb258985cdf
Art Rosnovsky <art.rosnovsky@auth0.com>
2022-05-06 11:58:44 -0700

    more optimizations

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require('@sentry/nextjs');
 
-const nextConfig = {
+module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
@@ -66,23 +65,3 @@ const securityHeaders = [
     value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
   },
 ];
-
-module.exports = nextConfig;
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  org: 'rosnovsky',
-  project: 'rosnovskyus',
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
