47c74897758acc6e4355c1cdddcf45f9a0ad81a3
Art Rosnovsky <art.rosnovsky@auth0.com>
2022-05-09 13:47:30 -0700

    add rss to home

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -14,6 +14,27 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <link
+            key="rss-feed"
+            rel="alternative"
+            type="application/rss+xml"
+            title="RSS feed for Rosnovsky.us"
+            href="/feed/feed.xml"
+          />
+          <link
+            key="atom-feed"
+            rel="alternative"
+            type="application/atom+xml"
+            title="Atom feed for Rosnovsky.us"
+            href="/feed/atom.xml"
+          />
+          <link
+            key="json-feed"
+            rel="alternative"
+            type="application/feed+json"
+            title="JSON feed for Rosnovsky.us"
+            href="/feed/feed.json"
+          />
         </Head>
         <body>
           <Main />
