7702cb7a94b05d3641d26f1dd1e3796c35727b92
Art Rosnovsky <artem@rosnovsky.us>
2022-05-27 05:59:57 +0000

    fix fonts #251

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,15 +5,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=optional"
-            rel="dns-prefetch"
-          />
-          <link
-            dns-prefetch
-            href="https://llama.rosnovsky.us/script.js"
             rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
+          <link href="https://llama.rosnovsky.us/script.js" rel="preconnect" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
