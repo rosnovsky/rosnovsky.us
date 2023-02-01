46f3edaf81c04d935385b495cebb91fd8f0bbd25
Art Rosnovsky <artem@rosnovsky.us>
2023-02-01 10:58:12 -0800

    update Okta logo

--- apps/web/src/images/logos/index.tsx
+++ apps/web/src/images/logos/index.tsx
@@ -1,6 +1,6 @@
 import logoAtt from './att.svg';
 import logoIntel from './intel.svg';
 import logoMicrosoft from './microsoft.svg';
-import logoOkta from './okta.svg';
+import logoOkta from './okta.png';
 
 export { logoAtt, logoIntel, logoMicrosoft, logoOkta}
