b829aff13287f4e762a3c60f15976347bcf10bd2
Art Rosnovsky <artem@rosnovsky.us>
2023-03-12 13:25:50 -0700

    chore: WIP styling

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'pnpm preview',
+    command: 'pnpm build && pnpm spreview',
     url: 'http://localhost:3000/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
