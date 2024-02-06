3da14980781957ec528e2d52e044922de87320b3
Art Rosnovsky <art@rosnovsky.us>
2024-02-06 14:40:57 -0800

    feat(blog): add blog post about resume automation
    Along with adding a new blog post, moved non-blog mdx files to a `todo` folder to get them out of
    the way. Added MDX support too.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,47 +5,38 @@ import remarkToc from 'remark-toc';
 import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
-
 import node from '@astrojs/node';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 4321,
-    host: true,
+    host: true
   },
   site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), sitemap(), mdx()],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: 'Table of contents',
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: 'Table of contents'
+    }]],
     shikiConfig: {
-      theme: 'nord',
+      theme: 'github-dark',
       wrap: true,
-      langs: ['javascript', 'typescript', 'bash', 'json', 'yaml'],
-    },
+      langs: ['javascript', 'typescript', 'bash', 'json', 'yaml', 'markdown']
+    }
   },
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
   scopedStyleStrategy: 'where',
   output: 'hybrid',
   adapter: node({
-    mode: 'standalone',
-  }),
+    mode: 'standalone'
+  })
 });
