9249780b2e621767368d82cb9c185bcdd6c928db
Art Rosnovsky <art@rosnovsky.us>
2024-04-20 11:59:56 -0700

    chore: trigger gh action

--- src/components/ViewsCount.tsx
+++ src/components/ViewsCount.tsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks';
 
-const ViewsCount = ({ currentPage }: { currentPage: string[] }): JSX.Element => {
-  const [views, setViews] = useState()
+const ViewsCount = ({
+  currentPage,
+}: {
+  currentPage: string[];
+}): JSX.Element => {
+  const [views, setViews] = useState();
 
   useEffect(() => {
     const fetchViews = async () => {
-      const visitsData = await fetch(`/visitors/count?page=${currentPage}`)
-      const visits = await visitsData.json()
+      const visitsData = await fetch(`/visitors/count?page=${currentPage}`);
+      const visits = await visitsData.json();
 
-      setViews(visits)
-    }
+      setViews(visits);
+    };
 
-    fetchViews()
-  }, [currentPage])
+    fetchViews();
+  }, [currentPage]);
 
   return (
-    <span className="mt-8 text-base text-slate-400/90 md:mt-0">Views: {views}</span>
+    <span className="mt-8 text-base text-slate-400/90 md:mt-0">
+      <a href="/blog/exploring-astro-db">Views</a> since Mar 13, 2024: {views}
+    </span>
   );
-}
+};
 
 export default ViewsCount;
