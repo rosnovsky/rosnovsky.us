8c4a2ddeda146335afacf93255bb80a8ed2e9977
Art Rosnovsky <art@rosnovsky.us>
2025-04-03 16:48:36 -0700

    feat: Merge feat/live-music-in-my-life (squashed)

--- .netlify/build/chunks/slugs_CqsLKNIA.mjs
+++
@@ -1,8 +0,0 @@
-function slugify(inputString) {
-  return inputString.replace(/ /g, "-").toLowerCase();
-}
-function reverseSlug(slug) {
-  return slug?.replace(/-/g, " ").split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
-}
-
-export { reverseSlug as r, slugify as s };
