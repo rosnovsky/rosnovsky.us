bbf4a20fd195af2a57eb5b5f15dc014b065d6d8f
Art Rosnovsky <art@rosnovsky.us>
2025-04-03 15:31:09 -0700

    chore: remove .netlify

--- .netlify/functions-serve/.unzipped/ssr/.netlify/build/chunks/exploring-astro-db_DvGTHlFG.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./exploring-astro-db_DXNOXE7N.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = ["[data-astro-image]{width:100%;height:auto;-o-object-fit:var(--fit);object-fit:var(--fit);-o-object-position:var(--pos);object-position:var(--pos);aspect-ratio:var(--w) / var(--h)}[data-astro-image=responsive]{max-width:calc(var(--w) * 1px);max-height:calc(var(--h) * 1px)}[data-astro-image=fixed]{width:calc(var(--w) * 1px);height:calc(var(--h) * 1px)}\n"];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
